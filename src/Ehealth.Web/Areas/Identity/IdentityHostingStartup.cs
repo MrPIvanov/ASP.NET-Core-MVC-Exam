331c1a3a2e98cc2694ae7a2ede14149678392017
MrPIvanov <MrPIvanov1117@gmail.com>
2019-08-19 11:00:49 +0300

    Scaffold all Pages

---
+++ src/Ehealth.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(Ehealth.Web.Areas.Identity.IdentityHostingStartup))]
+namespace Ehealth.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
