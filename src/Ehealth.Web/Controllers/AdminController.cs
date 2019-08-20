2d29acc47ca9f09a41ea30d3faf6281cc4fc9ace
MrPIvanov <MrPIvanov1117@gmail.com>
2019-08-20 09:29:28 +0300

    Added all admin view (empty). Ready for implementation

--- src/Ehealth.Web/Controllers/AdminController.cs
+++ src/Ehealth.Web/Controllers/AdminController.cs
@@ -9,5 +9,81 @@ namespace Ehealth.Web.Controllers
         {
             return this.View();
         }
+
+
+        // PRODUCTS
+
+
+        public async Task<IActionResult> ProductAddNew()
+        {
+            return this.View();
+        }
+
+        public async Task<IActionResult> ProductOrder()
+        {
+            return this.View();
+        }
+
+        public async Task<IActionResult> ProductEdit()
+        {
+            return this.View();
+        }
+
+        public async Task<IActionResult> ProductInfo()
+        {
+            return this.View();
+        }
+
+        public async Task<IActionResult> ProductDeleted()
+        {
+            return this.View();
+        }
+
+
+        // CATEGORIES
+
+
+        public async Task<IActionResult> CategoryAddNew()
+        {
+            return this.View();
+        }
+
+        public async Task<IActionResult> CategoryInfo()
+        {
+            return this.View();
+        }
+
+
+        // USERS
+
+
+        public async Task<IActionResult> UserInfo()
+        {
+            return this.View();
+        }
+
+        public async Task<IActionResult> UserAdmins()
+        {
+            return this.View();
+        }
+
+        public async Task<IActionResult> UserUpdateRole()
+        {
+            return this.View();
+        }
+
+
+        // SALES
+
+
+        public async Task<IActionResult> SalesLastMonth()
+        {
+            return this.View();
+        }
+
+        public async Task<IActionResult> SalesAll()
+        {
+            return this.View();
+        }
     }
 }
