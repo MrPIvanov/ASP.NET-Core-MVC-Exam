d24fc4e9635ed41f81d16500df498ab8e7523157
MrPIvanov <MrPIvanov1117@gmail.com>
2019-08-28 13:55:57 +0300

    Added MessageServiceTests.

--- src/Ehealth.Tests/Services/MessageServiceTests.cs
+++ src/Ehealth.Tests/Services/MessageServiceTests.cs
@@ -1,6 +1,39 @@
-﻿namespace Ehealth.Tests.Services
+﻿using Ehealth.Data;
+using Ehealth.Services;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ehealth.Tests.Services
 {
     public class MessageServiceTests
     {
+        private readonly EhealthDbContext context;
+
+        public MessageServiceTests()
+        {
+            this.context = EhealthDbContextInMemoryFactory.InitializeInMemoryContext();
+        }
+
+        [Fact]
+        public async Task CreateMessageForUserAndAddItToDbTests()
+        {
+            //Arrange
+            var messageService = new MessageService(this.context);
+
+            var userId = "TestUserId";
+
+            var message = "TestMessage";
+
+            //Act
+            Assert.Equal(0, this.context.Messages.Count());
+
+            await messageService.CreateMessageForUserAndAddItToDb(userId, message);
+            await messageService.CreateMessageForUserAndAddItToDb(userId, message);
+            await messageService.CreateMessageForUserAndAddItToDb(userId, message);
+
+            //Assert
+            Assert.Equal(3, this.context.Messages.Count());
+        }
     }
 }
